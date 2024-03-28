9ea54a3294a24d51ce46b049de2f85b5438d0771
renming <1689119010@qq.com>
2024-03-28 11:48:58 +0800

    测试

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mw/work/LiDAR_IMU_Init
-# Build directory: /home/mw/work/LiDAR_IMU_Init/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lidar_imu_init__py")
